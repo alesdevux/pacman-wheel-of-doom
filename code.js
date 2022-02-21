2f39be6963d21b7460bd4b1f6ff03569f4948069
Ales <aleswebgit@gmail.com>
2022-02-21 15:13:38 +0100

    add reload function to print in html, and reset all

--- code.js
+++ code.js
@@ -1,37 +1,76 @@
+const addPerson = document.getElementById('add-person');
+const addPersonBtn = document.getElementById('add-person-btn');
+const active = document.getElementById('active');
+const inactive = document.getElementById('inactive');
+const reset = document.getElementById('reset');
 const deleteBtn = document.getElementById('delete');
 
-let defaulPersonsList = [
-  'Jess',
-  'Daniel',
-  'Ales',
-  'Àlex',
-  'Scarlet',
-  'Albert',
-  'Liviu',
-  'Abde',
-  'Ingrid',
-  'Uri',
-  'Sergi',
-  'Yeray',
-  'Guillermo',
-];
-let deletePersonsList = [];
+let personsList = JSON.parse(localStorage.getItem('personsList'));
+let deletePersonsList = JSON.parse(localStorage.getItem('deletePersonsList'));
 
-deleteBtn.addEventListener('click', () => {
-  deleteRandomPerson();
-});
+deleteBtn.addEventListener('click', deleteRandomPerson);
+addPersonBtn.addEventListener('click', addPersonToList);
+reset.addEventListener('click', resetList);
+
+reloadAll();
+
+function reloadAll() {
+  // location.reload();
+
+  if (personsList === null || personsList.length === 0) {
+    active.innerHTML = `<li>No persons active</li>`;
+  } else {
+    active.innerHTML = '';
+    for (let i = 0; i < personsList.length; i++) {
+      active.innerHTML += `<li>${personsList[i]}</li>`;
+    }
+  }
+  
+  if (deletePersonsList === null || deletePersonsList.length === 0) {
+    inactive.innerHTML = `<li>No persons deth</li>`;
+  } else {
+    inactive.innerHTML = '';
+    for (let i = 0; i < deletePersonsList.length; i++) {
+      inactive.innerHTML += `<li>${deletePersonsList[i]}</li>`;
+    }
+  }
+}
+
+function addPersonToList() {
+  let person = addPerson.value;
+  if (personsList === null) {
+    personsList = [];
+  }
+  if (person !== '') {
+    personsList.push(person);
+    localStorage.setItem('personsList', JSON.stringify(personsList));
+    addPerson.value = null;
+    console.log('After add: ' + personsList);
+  }
+
+  reloadAll();
+}
 
 function deleteRandomPerson() {
-  console.log('Before delete: ' + defaulPersonsList);
+  let randomPerson = personsList[Math.floor(Math.random() * personsList.length)];
+  personsList.splice(personsList.indexOf(randomPerson), 1);
 
-  let randomPerson = defaulPersonsList[Math.floor(Math.random() * defaulPersonsList.length)];
-  defaulPersonsList.splice(defaulPersonsList.indexOf(randomPerson), 1);
-
+  if (deletePersonsList === null) {
+    deletePersonsList = [];
+  }
   deletePersonsList.push(randomPerson);
-
-  console.log('Delete person: ' + randomPerson);
-  console.log('After delete: ' + deletePersonsList);
   // save to local storage
   localStorage.setItem('deletePersonsList', JSON.stringify(deletePersonsList));
-  localStorage.setItem('defaulPersonsList', JSON.stringify(defaulPersonsList));
+  localStorage.setItem('personsList', JSON.stringify(personsList));
+
+  reloadAll();
+}
+
+function resetList() {
+  personsList = [];
+  deletePersonsList = [];
+  localStorage.setItem('personsList', JSON.stringify(personsList));
+  localStorage.setItem('deletePersonsList', JSON.stringify(deletePersonsList));
+  
+  reloadAll();
 }
